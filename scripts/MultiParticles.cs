fbc3a59652fd828de4842ff61a59f3e548c99cb1
Marinho Brandao <marinho@gmail.com>
2023-12-14 08:33:49 +0100

    Hexagon mosaic, implosion, shockwave

--- scripts/MultiParticles.cs
+++ scripts/MultiParticles.cs
@@ -1,65 +1,60 @@
-using System;
 using Godot;
+using System;
 
-/**
- * MultiParticles : Node3D
- *
- * This class turns possible to control multiple particles in sync.
- * Inspired by https://www.reddit.com/r/godot/comments/181ui9c/comment/kaewcca/?context=3
- */
 public partial class MultiParticles : Node3D
 {
-    [Export]
-    public bool Emitting = true;
+	[Export]
+	public bool Emitting = true;
 
-    [Export]
-    public bool OneShot = false;
+	[Export]
+	public bool OneShot = false;
 
-    [Export]
-    public Godot.Collections.Array<GpuParticles3D> Particles;
+	[Export]
+	public Godot.Collections.Array<GpuParticles3D> Particles;
 
-    double Lifetime = 0.0f;
+	double Lifetime = 0.0f;
 
-    public override void _Ready()
-    {
-        if (Emitting)
-        {
-            StartEmitters();
-        }
-    }
+	public override void _Ready()
+	{
+		if (Emitting)
+		{
+			StartEmitters();
+		}
+	}
 
-    void GetLifetime()
-    {
-        foreach (var particle in Particles)
-        {
-            if (particle.Lifetime > Lifetime)
-            {
-                Lifetime = particle.Lifetime;
-            }
-        }
-    }
+	void GetLifetime()
+	{
+		foreach (var particle in Particles)
+		{
+			if (particle.Lifetime > Lifetime)
+			{
+				Lifetime = particle.Lifetime;
+			}
+		}
+	}
 
-    public async void StartEmitters()
-    {
-        GetLifetime();
+	public async void StartEmitters()
+	{
+		GetLifetime();
 
-        foreach (var particle in Particles)
-            particle.Emitting = true;
+		foreach (var particle in Particles)
+			particle.Emitting = true;
 
-        await ToSignal(GetTree().CreateTimer(Lifetime), SceneTreeTimer.SignalName.Timeout);
-        DisposeOfEmitters();
+		await ToSignal(GetTree().CreateTimer(Lifetime), SceneTreeTimer.SignalName.Timeout);
+		DisposeOfEmitters();
 
-        if (!OneShot)
-        {
-            StartEmitters();
-        }
-    }
+		if (!OneShot)
+		{
+			StartEmitters();
+		}
+	}
 
-    void DisposeOfEmitters()
-    {
-        foreach (var particle in Particles)
-        {
-            particle.Emitting = false;
-        }
-    }
+	void DisposeOfEmitters()
+	{
+		foreach (var particle in Particles)
+		{
+			particle.Emitting = false;
+		}
+	}
+
 }

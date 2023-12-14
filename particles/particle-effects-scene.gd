c711af00ecd25b5cd718d2d59c7f049bf67a421f
Mario Brandao <marinho@gmail.com>
2023-12-14 22:10:23 +0100

    Particles with a menu

---
+++ particles/particle-effects-scene.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+@onready var labelVisibleEffects = %LabelVisibleEffects
+@onready var buttonSmoke = %ButtonSmoke
+@onready var effectsContainer = %Effects
+@onready var sideBarMenu = $"CanvasLayer UI/SideBar/SideBar Menu"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func updateVisibleEffects():
+	var pressedButton = buttonSmoke.button_group.get_pressed_button()
+	var effect = get_node("Effects/" + pressedButton.text.strip_edges())
+
+	for child in effectsContainer.get_children():
+		if child == effect:
+			child.show()
+		else:
+			child.hide()
+
+	labelVisibleEffects.text = "[center]" + pressedButton.text
+
+
+func toggleSideBar():
+	if sideBarMenu.visible: # Replace with function body.
+		sideBarMenu.hide()
+	else:
+		sideBarMenu.show()
+	
